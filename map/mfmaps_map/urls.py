aede61310b0566c1c4d1bdcf32e047c49e281829
Nick Arnold <nicholas@nicholasdarnold.com>
2026-05-06 17:14:47 -0700

    add robots.txt blocking crawlers (pre-launch)

--- map/mfmaps_map/urls.py
+++ map/mfmaps_map/urls.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'home.html')
 
 
+def robots_txt(request):
+    return HttpResponse(
+        "User-agent: *\nDisallow: /\n",
+        content_type="text/plain",
+    )
+
+
 urlpatterns = [
     path('', home, name='home'),
+    path('robots.txt', robots_txt, name='robots'),
     path('admin/', admin.site.urls),
 ]
