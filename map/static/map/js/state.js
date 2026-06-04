9ba989daac9703b26be9284191b7142a6070b5a2
Nick Arnold <nicholas@nicholasdarnold.com>
2026-06-04 11:24:49 -0700

    collapse hydro into a single switch

--- map/static/map/js/state.js
+++ map/static/map/js/state.js
@@ -28,26 +28,24 @@ export const LAYER_IDS = {
         'hawaii-z11-12-hillshade'
     ],
     'hydrography': [
+        // Protomaps labels
         'nhd-streams-label-large',
         'nhd-streams-label-medium',
         'nhd-streams-label-small',
         'nhd-waterbodies-label',
-    ],
-    'nhd_ak': [
+        // AK
         'nhd-ak-streams',
         'nhd-ak-waterbodies-fill',
         'nhd-ak-waterbodies-stroke',
         'nhd-ak-streams-label-high',
         'nhd-ak-streams-label-mid',
         'nhd-ak-streams-label-low',
-        'nhd-ak-waterbodies-label'
-    ],
-    'nhd_conus': [
+        'nhd-ak-waterbodies-label',
+        // CONUS
         'nhd-conus-streams',
         'nhd-conus-waterbodies-fill',
-        'nhd-conus-waterbodies-stroke',
-        'nhd-conus-waterbodies-label'
-    ],
+        'nhd-conus-waterbodies-stroke'
+    ]
 };
 
 export const H3_RES = 8;
