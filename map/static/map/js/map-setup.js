38fdaeab2131338f149bcdb0ad919755b938fc69
Nick Arnold <nicholas@nicholasdarnold.com>
2026-07-13 16:02:50 -0700

    Fix stale JS on user browsers after deploy, test 2

--- map/static/map/js/map-setup.js
+++ map/static/map/js/map-setup.js
@@ -79,7 +79,7 @@ function wireUrlSync() {
 
 export function initMap() {
 
-    console.log('map-setup.js loaded — version 1');
+    console.log('map-setup.js loaded — version 2');
 
     if (!state._pmtilesRegistered) {
         const protocol = new pmtiles.Protocol();
