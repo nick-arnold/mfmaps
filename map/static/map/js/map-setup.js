02bdb4ef3051a14a6b83cd4b8a9f4595003ad3e9
Nick Arnold <nicholas@nicholasdarnold.com>
2026-07-13 16:43:38 -0700

    Fix stale html, check v5

--- map/static/map/js/map-setup.js
+++ map/static/map/js/map-setup.js
@@ -79,7 +79,7 @@ function wireUrlSync() {
 
 export function initMap() {
 
-    console.log('map-setup.js loaded — version 4');
+    console.log('map-setup.js loaded — version 5');
 
     if (!state._pmtilesRegistered) {
         const protocol = new pmtiles.Protocol();
