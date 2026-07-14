150feabe1fd175ab329ac44f9eb10622b0bba4a5
Nick Arnold <nicholas@nicholasdarnold.com>
2026-07-13 17:29:33 -0700

    trying another route to prevent stale js, v12

--- map/static/map/js/map-setup.js
+++ map/static/map/js/map-setup.js
@@ -79,7 +79,7 @@ function wireUrlSync() {
 
 export function initMap() {
 
-    console.log('map-setup.js loaded — version 11');
+    console.log('map-setup.js loaded — version 12');
 
     if (!state._pmtilesRegistered) {
         const protocol = new pmtiles.Protocol();
